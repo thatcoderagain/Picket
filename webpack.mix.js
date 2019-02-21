01c35b72feeddf78a0a58abc6b29d9ba39bbbc2e
Gaurav Kumar <gaurav.officialsites@gmailcom>
2019-02-21 20:24:59 +0530

    Navbar Fixed

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').sass('resources/scss/app.scss', 'public/scss');
+mix.js('resources/js/app.js', 'public/js').sass('resources/scss/app.scss', 'public/css');
 
 // Full API
 // mix.js(src, output);
