a53d5ede8cae34a7c84745620f14522557f09548
Ishita <ishitactaneja@gmail.com>
2019-02-21 22:22:47 +0530

    Ishtia

---
+++ resources/js/views/Slider.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="carousel-item">
+    abc
+    <img src="src" alt="carousel-item">
+    <div class="carousel-caption d-none d-md-block">
+        <h5>{{title}}</h5>
+        <p>{{msg}}</p>
+    </div>
+</div>
+</template>
+
+<script>
+  export default {
+    props: ['src', 'title', 'msg'],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        
+    }
+    }
+</script>
