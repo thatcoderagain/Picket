012a8cb274e78e3f8b4d11e9fa0b7fadfad7a9f3
ankita <ankig95@gmail.com>
2019-02-24 20:08:12 +0530

    plan page

---
+++ resources/js/views/Plans.vue
@@ -0,0 +1,49 @@
+<template>
+ <div>
+    <div class="card-fluid">
+      <img src="http://wallpoper.com/images/00/36/92/04/outer-space_00369204.jpg" class="card-img-top" alt="...">
+      <div class="centered">Have we got a plan for you.<br/>Free 14-day trial.</div>
+    </div>
+    <h3>Plans at a glance.</h3>
+    <p> Unlimited uploads come standard for every Picket plan along with all the tools you need to create a beautiful photo site, share easily, and order personalized prints and gifts. Each plan is designed to protect and preserve the photos you care about: which means all of them.</p>
+    <div class="card-group ml-5 mr-5 mt-5 mb-5">
+      <div class="card">
+        <div class="card-body">
+         <h5 class="card-title" style="text-align: center;"><b>Basic</b></h5>
+         <p class="card-text">$47.88/year </p>
+         <p class="card-text"><b>Save 33%</b> when billed annually.Or $5.99 monthly</p>
+         <p class="card-text"> Perfect for you if you’re simply looking for a beautiful, safe home for your photos.</p>
+        </div>
+      </div>
+    <div class="card">
+       <div class="card-body">
+       <h5 class="card-title" style="text-align: center"><b>Power</b></h5>
+       <p class="card-text">$71.88/year</p>
+       <p class="card-text"><b>Save 33%</b> when billed annually.Or $8.99 monthly.</p>
+       <p class="card-text">Perfect for you if you’re interested in advanced control over the way your photo site looks.</p>
+       </div>
+    </div>
+    <div class="card">
+       <div class="card-body">
+       <h5 class="card-title" style="text-align: center"><b>Pro</b></h5>
+       <p class="card-text">$179.88/year</p>
+       <p class="card-text"><b>Save 38%</b> when billed annually.Or $23.99 monthly.</p>
+       <p class="card-text">Perfect if you are a professional photographer and need a robust workflow and client tools.</p>
+
+      </div>
+    </div>
+    </div>
+ </div>
+</template>
+<style>
+.centered {
+  color: white;
+  font-size: 45px;
+  font-family: "Georgia";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+</style>
