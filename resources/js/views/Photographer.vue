47be6bd13bb0cb78de757ebfd3e7cd825f273b1e
Gaurav Kumar <gaurav.officialsites@gmailcom>
2019-03-25 23:54:55 +0530

    same

---
+++ resources/js/views/Photographer.vue
@@ -0,0 +1,60 @@
+<style>
+.containerProfile
+{
+    background-size: cover;
+    background-color: rgba(0, 0, 0, 1.0);
+    min-width: 100%;
+    padding: 16px;
+}
+.bottomMargin {
+    margin-bottom: 20px;
+}
+.nameFont {
+    font-family: -webkit-pictograp;
+    font-size:35px;
+    color: white;
+}
+.quotes {
+    font-size:20px;
+    display: inline;
+}
+</style>
+
+<template>
+    <div>
+        <div class="containerProfile">
+            <div class="teacher-name" style="padding-top:20px;">
+                <div class="row" style="margin-top:0px;">
+                    <div class="col-md-9">
+                        <h2><strong class="nameFont">Ishita Taneja</strong></h2>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="button" style="float:right;">
+                            <a href="#" class="btn btn-outline-success btn-sm">Edit Profile</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row" style="margin-top:20px;">
+                <div class="col-md-3">
+                  <a href="#"> <img class="rounded-circle bottomMargin" src="https://as.ftcdn.net/r/v1/pics/ea2e0032c156b2d3b52fa9a05fe30dedcb0c47e3/landing/images_photos.jpg" alt="Kamal" style="width:200px;height:200px"></a>
+              </div>
+
+              <div class="col-md-6">
+                    <h5 style="color:#3AAA64">Assistant Photographer, <small>Dept. of Photography, CSA University</small></h5>
+                    <p style="color: white ; font-size:18px;"><div style="text-align: left; color: white; font-size:18px;"><i>"Patient observer of the stars. Grinning wild-child of the mountain and its rivers. Adventurous spirit with camera in hand. Captures moments of wonder and serenity. Based in Portland, OR, Isaac is an advertising photographer/director with a focus in adventure, automotive, and active lifestyle."</i></div></p>
+              </div>
+
+              <div class="col-md-3 text-center">
+                  <span class="number" style="font-size:18px; color:white">Phone:<strong> +8801732226402</strong></span>
+                  <span class="number" style="font-size:18px; color:white; display: block;">Location:<strong> Chicago, USA</strong></span>
+                  <div class="button" style="padding-top:18px">
+                    <a href="mailto:ahmkctg@yahoo.com" class="btn btn-outline-success btn-block">Send Email</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+</template>
